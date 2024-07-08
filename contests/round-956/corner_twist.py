617f9be90cd8d9d12217019b42f36c8db6463fed
khayliang <khayliang@gmail.com>
2024-07-08 12:54:44 +0800

    round 956

--- contests/round-956/corner_twist.py
+++ contests/round-956/corner_twist.py
@@ -18,52 +18,9 @@ def invr():
 def outlt(a):
     return " ".join(map(str, a))
 
-def get_prefix_sum(a):
-    a_sm = [0]
-
-    for x in a:
-        a_sm.append(a[-1] + x)
-
-    return a_sm
-
-def window(a_pf, b_pf, c_pf):
-    mx = 0
-    prev = 0
-    l = 1
-
-    # inclusive
-    for r in range(1, n):
-        while l != r:
-            a_sm = a_pf[r + 1] - a_pf[l]
-            bc_sm = b_pf[l] + c_pf[-1] - c_pf[r + 1]
-            cb_sm = c_pf[l] + b_pf[-1] - b_pf[r + 1]
-
-            if a_sm + bc_sm < 
-
-
 
 # use yield to give ans. return to stop
 def solve():
-    n = inp()
-    a = inlt()
-    b = inlt()
-    c = inlt()
-
-    a_sm = get_prefix_sum(a)
-    b_sm = get_prefix_sum(b)
-    c_sm = get_prefix_sum(c)
-
-    mx = 0
-
-   
-
-        
-
-        
-
-
-        
-
 
 def test():
     ans = []
