0db4152935d26cef3f88af802615b10ce667fbba
khayliang <khayliang@gmail.com>
2024-08-06 23:00:43 +0800

    060824 prac

---
+++ practice/pinely4_b.py
@@ -0,0 +1,91 @@
+import sys
+import math
+import heapq
+import bisect
+from collections import deque, defaultdict
+from itertools import permutations
+from types import GeneratorType
+
+input = sys.stdin.readline
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+############ ---- Output Functions ---- ############
+def outlt(a):
+    return " ".join(map(str, a))
+
+############ ---- Constants ---- ############
+INF = float('inf')
+
+############ ---- Function to Bootstrap Recursion ---- ############
+"""
+Add the decorator @bootstrap above the recursive function.
+For the recursion to work, yield the recusive function.
+Recursive function must yield something regardless
+To return a value, add the line yield yield fn()
+"""
+def bootstrap(f, stack=deque()):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+    
+
+# use yield to give ans. return to stop
+def solve():
+    n = inp()
+    b = inlt()
+
+    a = [b[0]]
+
+    for i in range(1, n - 1):
+        a.append(b[i] | b[i - 1])
+        if a[-1] & a[-2] != b[i - 1]:
+            yield - 1
+            return
+        
+    a.append(b[-1])
+    if a[-1] & a[-2] != b[-1]:
+            yield - 1
+            return
+
+
+    yield outlt(a)
+
+
+
+def test():
+    ans = []
+    for _ in range(inp()):
+        for a in solve():
+            ans.append(a)
+    for i in ans:
+        print(i)
+
+def submit():
+    for _ in range(inp()):
+        for a in solve():
+            print(a)
+
+submit()
---
+++ practice/pinely4_c.py
@@ -0,0 +1,89 @@
+import sys
+import math
+import heapq
+import bisect
+from collections import deque, defaultdict
+from itertools import permutations
+from types import GeneratorType
+
+input = sys.stdin.readline
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+############ ---- Output Functions ---- ############
+def outlt(a):
+    return " ".join(map(str, a))
+
+############ ---- Constants ---- ############
+INF = float('inf')
+
+############ ---- Function to Bootstrap Recursion ---- ############
+"""
+Add the decorator @bootstrap above the recursive function.
+For the recursion to work, yield the recusive function.
+Recursive function must yield something regardless
+To return a value, add the line yield yield fn()
+"""
+def bootstrap(f, stack=deque()):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+    
+
+# use yield to give ans. return to stop
+def solve():
+    n = inp()
+    a = inlt()
+    
+    evens = 0
+    for x in a:
+        if x % 2 == 0:
+            evens += 1
+    if evens != 0 and evens != n:
+        yield -1
+        return
+
+    res = [1 << i for i in range(29, -1, -1)]
+
+    if evens != 0:
+        res.append(1)
+    
+    yield len(res)
+    yield outlt(res)
+
+def test():
+    ans = []
+    for _ in range(inp()):
+        for a in solve():
+            ans.append(a)
+    for i in ans:
+        print(i)
+
+def submit():
+    for _ in range(inp()):
+        for a in solve():
+            print(a)
+
+test()
